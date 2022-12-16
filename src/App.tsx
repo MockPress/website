3ed9c3829c016940947ebd51c87d5bef217a8ae2
airman5573 <rpf5573@kakao.com>
2022-12-16 17:30:32 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./main.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex bg-slate-100 h-20">
+      <span className="">HELLO</span>
+    </div>
+  );
+};
+
+export default App;
